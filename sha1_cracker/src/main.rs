6ce5bc72a0cbfe28c75cb997286eca811b9c91f7
Willian <willsbit@gmail.com>
2022-06-24 14:10:33 -0300

    [FORMAT] cargo fmt

--- sha1_cracker/src/main.rs
+++ sha1_cracker/src/main.rs
@@ -1,5 +1,10 @@
 use sha1::Digest;
-use std::{env, error::Error, fs::File, io::{BufRead, BufReader}};
+use std::{
+    env,
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
 
 const SHA1_HEX_STRING_LENGTH: usize = 40;
 
