4dc12b2d5f2758a9526bbdd8226e22f120b2cf59
Willian <willsbit@gmail.com>
2022-06-25 16:50:47 -0300

    [FEATURE] Create Spider trait

---
+++ web_crawler/src/spiders/mod.rs
@@ -0,0 +1,11 @@
+use crate::error::Error;
+
+#[async_trait]
+pub trait Spider: Send + Sync {
+    type Item;
+
+    fn name(&self) -> String;
+    fn start_urls(&self) -> Vec<String>;
+    async fn scrape(&self, url: String) -> Result<(Vec<Self::Item>, Vec<String>), Error>;
+    async fn process(&self, item: Self::Item) -> Result<(), Error>;
+}
